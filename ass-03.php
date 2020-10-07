f801faa3490ffd0f21127f8cd9f885c2edbf068f
pacharacamt <pachara.t@cmu.ac.th>
2020-10-07 05:05:36 +0000

    Please correct assignment 3. I change input file for pointing you to the problems.

--- ass-03.php
+++ ass-03.php
@@ -7,8 +7,17 @@
 $fp=fopen($_SERVER['argv'][1],"r");
 $datas=[];
 
+/**
+ * Using preg_split() seem to be a good idea but it cannot
+ * handle the case of capital letters start from the begining
+ * of line and follow by ( or ) for example:
+ * XXX(just test)
+ * In this case you will get XXX in your result.
+ */
 while(! feof($fp)){
-    $data=preg_split('/(\()|(\))/',trim(fgets($fp)),$n);
+    // $n is not necessary.
+    //$data=preg_split('/(\()|(\))/',trim(fgets($fp)),$n);
+    $data=preg_split('/(\()|(\))/',trim(fgets($fp)));
     $datas[]=$data;
 }
 
